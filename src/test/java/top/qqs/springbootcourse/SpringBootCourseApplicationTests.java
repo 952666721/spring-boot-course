ccdb2d3d191cd5ac298b947320eb0a5ba559af51
qqs <952666721@qq.com>
2025-09-04 08:40:17 +0800

    init: 初始化 spring-boot-course 项目

---
+++ src/test/java/top/qqs/springbootcourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package top.qqs.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
